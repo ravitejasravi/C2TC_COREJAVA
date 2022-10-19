5d0b37d4cd6cb792bd36077b56af90547e8acb7d
ravitejasravi <ravitejats18@gmail.com>
2022-10-19 22:47:04 +0530

    initial

---
+++ ArithmeticOperators/src/AllInOne.java
@@ -0,0 +1,22 @@
+
+public class AllInOne {
+
+	public static void main(String[] args) {
+		       
+	        int num1 = 10, num2 = 20; 
+	 
+	        System.out.println("Substraction = " + (num2 - num1)); 
+	        System.out.println("Add = " + (num2 + num1));
+	        System.out.println("Mul = " + (num2 * num1));
+	        System.out.println("Div = " + (num2 / num1));
+	        System.out.println("Mod = " + (num2 % num1));
+	        
+	        System.out.println("Post Incremenet = " + num1++);
+	        System.out.println("Post Incremenet = " + num1);
+	        
+	        System.out.println("Pre Incremenet = " + (++num1));
+	        System.out.println("Pre Incremenet = " + num1);
+	       
+	}
+
+}
