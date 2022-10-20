ed7b3b54fa1ff1e63465fb0babe42cf7ab021b5c
ravitejasravi <ravitejats18@gmail.com>
2022-10-20 06:21:05 +0530

    initial

---
+++ BreakAndContinue/src/Break.java
@@ -0,0 +1,15 @@
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+		      if (i == 3) {
+		        break;
+		      }
+		      System.out.println(i);
+		    }  
+
+	}
+
+}
